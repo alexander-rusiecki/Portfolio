8d8b3154294f44810663bc0eb1cc83a1b31f5cb2
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-11-14 09:48:37 +0100

    add aria info to hamburger button and add some more comments to various files

--- js/main.js
+++ js/main.js
@@ -12,14 +12,12 @@ let w = (canvas.width = window.innerWidth),
     sparkChance: 0.1,
     sparkDist: 10,
     sparkSize: 2,
-
     color: 'hsla(hue,100%,light%,.05)',
     baseLight: 50,
     addedLight: 10,
     shadowToTimePropMult: 6,
     baseLightInputMultiplier: 0.01,
     addedLightInputMultiplier: 0.02,
-
     cx: w / 2,
     cy: h / 2,
     repaintAlpha: 0.04,
@@ -30,24 +28,18 @@ let w = (canvas.width = window.innerWidth),
   dieX = w / 2 / opts.len,
   dieY = h / 2 / opts.len,
   baseRad = (Math.PI * 2) / 6;
-
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, w, h);
-
 function loop() {
   window.requestAnimationFrame(loop);
-
   ++tick;
-
   ctx.globalCompositeOperation = 'source-over';
   ctx.shadowBlur = 0;
   ctx.fillStyle = 'rgba(0,9,20,alp)'.replace('alp', opts.repaintAlpha);
   ctx.fillRect(0, 0, w, h);
   ctx.globalCompositeOperation = 'lighter';
-
   if (lines.length < opts.count && Math.random() < opts.spawnChance)
     lines.push(new Line());
-
   lines.map(function (line) {
     line.step();
   });
@@ -60,29 +52,22 @@ Line.prototype.reset = function () {
   this.y = 0;
   this.addedX = 0;
   this.addedY = 0;
-
   this.rad = 0;
-
   this.lightInputMultiplier =
     opts.baseLightInputMultiplier +
     opts.addedLightInputMultiplier * Math.random();
-
   this.color = opts.color.replace('hue', tick * opts.hueChange);
   this.cumulativeTime = 0;
-
   this.beginPhase();
 };
 Line.prototype.beginPhase = function () {
   this.x += this.addedX;
   this.y += this.addedY;
-
   this.time = 0;
   this.targetTime = (opts.baseTime + opts.addedTime * Math.random()) | 0;
-
   this.rad += baseRad * (Math.random() < 0.5 ? 1 : -1);
   this.addedX = Math.cos(this.rad);
   this.addedY = Math.sin(this.rad);
-
   if (
     Math.random() < opts.dieChance ||
     this.x > dieX ||
@@ -95,14 +80,11 @@ Line.prototype.beginPhase = function () {
 Line.prototype.step = function () {
   ++this.time;
   ++this.cumulativeTime;
-
   if (this.time >= this.targetTime) this.beginPhase();
-
   var prop = this.time / this.targetTime,
     wave = Math.sin((prop * Math.PI) / 2),
     x = this.addedX * wave,
     y = this.addedY * wave;
-
   ctx.shadowBlur = prop * opts.shadowToTimePropMult;
   ctx.fillStyle = ctx.shadowColor = this.color.replace(
     'light',
@@ -116,7 +98,6 @@ Line.prototype.step = function () {
     2,
     2
   );
-
   if (Math.random() < opts.sparkChance)
     ctx.fillRect(
       opts.cx +
@@ -132,20 +113,16 @@ Line.prototype.step = function () {
     );
 };
 loop();
-
 window.addEventListener('resize', function () {
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, w, h);
-
   opts.cx = w / 2;
   opts.cy = h / 2;
-
   dieX = w / 2 / opts.len;
   dieY = h / 2 / opts.len;
 });
-
 
 // Handles navigation expansion/contraction and toggles aria-label value
 const hamburgerButton = document.querySelector('.hamburger-button');
@@ -155,4 +132,7 @@ hamburgerButton.addEventListener('click', () => {
     hamburgerButton.getAttribute('aria-label') === 'Open the menu'
       ? hamburgerButton.setAttribute('aria-label', 'Close the menu')
       : hamburgerButton.setAttribute('aria-label', 'Open the menu');
+  hamburgerButton.getAttribute('aria-expanded') === 'false'
+    ? hamburgerButton.setAttribute('aria-expanded', 'true')
+    : hamburgerButton.setAttribute('aria-expanded', 'false');
 });
--- scss/README.md
+++ scss/README.md
@@ -1,9 +1,9 @@
 # SASS folder structure:
 
-## based on the [7-1 architechture](https://sass-guidelin.es/#architecture) scaled down to 4-1 in this project.
+## Based on the [7-1 architechture](https://sass-guidelin.es/#architecture) scaled down to 4-1 in this project.
 
 - abstracts folder contains variables and mixins
 - base folder contains basic styles and helpers
 - layout folder contains main styles
 - vendors folder contains third party reset styles
-- main.scss file contains all imports and is compiled to main.min.css file in css folder
+- main.scss file contains all imports and is compiled to main.min.css file (in css folder)
